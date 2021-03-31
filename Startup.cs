8eb22875eaa3924899637374b0561ebb08e21f12
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-03-31 16:40:06 +0200

    created a basic MVC structure with hardcoded todo items

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,6 +36,9 @@ namespace AspNetCoreTodo
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
